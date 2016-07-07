b39fa017cb1a046590b390aae651a3ded463292e
gerardolsj <lsjgerardo@gmail.com>
2016-07-07 11:50:30 -0500

    Save Delete Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   moduleId: module.id,
@@ -10,10 +11,24 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 export class AppComponent {
 
    items: FirebaseListObservable<any[]>;
+   itemObj: FirebaseObjectObservable<any>;
 
   constructor(af: AngularFire) {
-    this.items = af.database.list('items');
+    this.items = af.database.list('/items');
+    this.itemObj = af.database.object('/obj');
+    console.log(this.itemObj);
+  }
+  save(newName: string) {
+    this.itemObj.set({ name: newName });
+  }
+  update(newSize: string) {
+    this.itemObj.update({ size: newSize });
+  }
+  delete() {
+    this.itemObj.remove();
   }
     
   title = 'app works!';
 }
+
+
