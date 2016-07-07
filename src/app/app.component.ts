525a1031c870603eb1114ee1c91252913db8b4ed
gerardolsj <lsjgerardo@gmail.com>
2016-07-07 16:16:01 -0500

    twitter display, add BOOTSTRAP and W3 css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,34 @@ import { FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   moduleId: module.id,
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-
+/*
    items: FirebaseListObservable<any[]>;
    itemObj: FirebaseObjectObservable<any>;
+   list: FirebaseListObservable<any[]>;
+*/
+   tweets: FirebaseListObservable<any[]>;
 
   constructor(af: AngularFire) {
+/*
     this.items = af.database.list('/items');
+    this.list = af.database.list('/messages');
     this.itemObj = af.database.object('/obj');
-    console.log(this.itemObj);
+     console.log(this.itemObj); 
+*/
+    this.tweets = af.database.list('/tweets');
   }
-  save(newName: string) {
+
+   changeState(key: string, newState: string) {
+    
+    this.tweets.update(key, {state: newState} );
+  }
+
+ /* save(newName: string) {
     this.itemObj.set({ name: newName });
   }
   update(newSize: string) {
@@ -26,9 +39,24 @@ export class AppComponent {
   }
   delete() {
     this.itemObj.remove();
+  }*/
+/*
+  add(newName: string) {
+    this.list.push({ text: newName });
   }
+  update(key: string, newSize: string) {
+    this.list.update(key, { text: newSize });
+  }
+  deleteItem(key: string) {    
+    this.list.remove(key); 
+  }
+  deleteEverything() {
+    this.list.remove();
+  }
+*/
     
   title = 'app works!';
+  subtitle = 'angularfire2 is the devil'
 }
 
 
